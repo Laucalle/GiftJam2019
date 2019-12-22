4d69a4ea73adfed32111b6ffc074fd85389bff09
Sergio <scmarquez@correo.ugr.es>
2019-12-22 13:19:45 +0100

    German scripts

---
+++ GiftJam2019/Assets/Scripts/German/MouseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit raycastHit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out raycastHit)) {
+                ShootingStarComponent shootingStarComponent = raycastHit.collider.GetComponent(typeof(ShootingStarComponent)) as ShootingStarComponent;
+                if(null != shootingStarComponent ) {
+                    shootingStarComponent.TrapStar();
+                }
+            }
+        }
+    }
+}
---
+++ GiftJam2019/Assets/Scripts/German/ShootingStarComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingStarComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float xAxisSpeed;
+    [SerializeField]
+    private float yAxisSpeed;
+
+    [SerializeField]
+    private float timeToDestroy;
+
+    public void Start()
+    {
+        Destroy();
+    }
+    public void Update()
+    {
+        Move();
+    }
+
+    public void TrapStar()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Move()
+    {
+        transform.Translate(new Vector3(xAxisSpeed*Time.deltaTime, yAxisSpeed*Time.deltaTime,0));
+    }
+    
+    private void Destroy()
+    {
+        Destroy(gameObject,timeToDestroy);
+    }
+}
